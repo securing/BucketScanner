6ff228d250c2be8eafcebb6437d95520ea08cac0
Pawel Rzepa <rzepsky@Sherman.local>
2018-02-28 11:12:48 +0100

    initial commit

---
+++ BucketScanner.py
@@ -0,0 +1,240 @@
+#!/bin/env python
+'''
+--------------
+BucketScanner
+By @Rzepsky
+--------------
+======================== Notes ========================
+This tool is made for legal purpose only!!! It allows you to:
+- find collectable files for an anonymous/authenticated user in your buckets
+- verify if an anonymous/authenticated user is allowed to upload arbitrary files to your buckets
+
+Example usage: ./BucketScanner.py -l bucket_list.txt -w upload_file.txt -r '^.*\.(db|sql)' -t 1000
+
+======================= Options =======================
+-l: specify a list with bucket names to check.
+-w: specify a file to upload to a bucket.
+-o: specify an output file.
+-r: specify a regular expression to filter the output.
+-s: look only for files bigger than 's' bytes 
+-m: look only for files smaller than 'm' bytes 
+-t: specify number of threads to use.
+-h: prints a help message.
+'''
+
+from argparse import ArgumentParser
+from threading import Thread, Lock
+import math
+import boto3
+import requests
+import Queue
+import re
+import sys
+
+queue = Queue.Queue()
+
+AWS_ACCESS_KEY_ID = ''
+AWS_SECRET_ACCESS_KEY = ''
+ANONYMOUS_MODE = False
+DISPLAY_SIZE = True
+WRITE_TEST_ENABLED = False
+WRITE_TEST_FILE = ""
+OUTPUT_FILE = "output.txt"
+MIN_SIZE = 1
+MAX_SIZE = 0
+REGEX = ".*"
+
+def setwriteTest(write_file):
+    global WRITE_TEST_ENABLED
+    global WRITE_TEST_FILE
+    WRITE_TEST_ENABLED = True
+    WRITE_TEST_FILE = write_file
+
+def setOutputFile(output_file):
+    global OUTPUT_FILE
+    OUTPUT_FILE = output_file
+
+def setMinsize(min_SIZE):
+    global MIN_SIZE
+    MIN_SIZE = min_SIZE
+
+def setMaxsize(max_SIZE):
+    global MAX_SIZE
+    MAX_SIZE = max_SIZE
+
+def setAnonymousMode():
+    global ANONYMOUS_MODE
+    ANONYMOUS_MODE = True
+    print "All tests will be executed in anonymous mode. If you want to send all requests using your AWS account, \
+    please specify AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY variables in {0} file".format(sys.argv[0])
+
+def setRegex(regex):
+    global REGEX
+    REGEX = regex
+
+def GetRegion(bucket_name):
+        try:
+            response = requests.get('http://'+bucket_name+'.s3.amazonaws.com/')
+            region = response.headers.get('x-amz-bucket-region')
+            return region
+        except Exception as e:
+            print "Error: couldn't connect to '{0}' bucket. Details: {1}".format(response, e)
+
+def getSession(bucket_name, region):
+    try:
+        if ANONYMOUS_MODE == True:
+            sess = boto3.session.Session(region_name=region)
+        else:
+            sess = boto3.session.Session(region_name=region, aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY)    
+        conn = sess.resource('s3')
+        bucket = conn.Bucket(bucket_name)
+        return bucket
+
+    except Exception as e:
+        print "Error: couldn't create a session with '{0}' bucket. Details: {1}".format(bucket_name, e)
+
+def getBucket(bucket_name):
+    region = GetRegion(bucket_name)
+    bucket = ""
+    if region == 'None':
+        print "Bucket '{0}' does not exist.".format(bucket_name.encode('utf-8'))
+    else:
+        bucket = getSession(bucket_name, region)
+    return bucket
+
+def size(size_bytes):
+   size_name = ("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB")
+   i = int(math.floor(math.log(size_bytes, 1024)))
+   p = math.pow(1024, i)
+   s = round(size_bytes / p, 2)
+   return "{0}{1}".format(s, size_name[i])
+
+def bucketReader(bucket_name):
+    region = GetRegion(bucket_name)
+    if region == 'None':
+        pass 
+    else:
+        print "Testing bucket {0}...".format(bucket_name)
+        bucket = getBucket(bucket_name)
+        results = "" 
+        try:      
+            for s3_object in bucket.objects.all():
+                try:
+                    if (MIN_SIZE < s3_object.get()["ContentLength"]) and \
+                     ((s3_object.get()["ContentLength"] < MAX_SIZE) if MAX_SIZE else True)\
+                     and re.match(REGEX, s3_object.key):
+                    
+                        item = "http://s3.{0}.amazonaws.com/{1}/{2}".format(\
+                                   region, bucket_name, \
+                                   s3_object.key.encode('utf-8'))
+                        results += item + '\n'
+                        print "Collectable: {0} {1}".format(item, size(s3_object.get()["ContentLength"]))         
+                except Exception as e:
+                     print "Error: couldn't get '{0}' object in '{1}' bucket. Details: {2}\n".format(s3_object.key.encode('utf-8'), bucket_name, e)
+            
+            appendOutput(results)
+        except Exception as e:
+            print "Error: couldn't access the '{0}' bucket. Details: {1}\n".format(bucket_name, e)
+
+def writeTest(bucket_name, filename):
+    region = GetRegion(bucket_name)
+    if region != 'None':
+        try:
+            data = open(filename, 'rb')
+            bucket = getBucket(bucket_name)
+            bucket.put_object(Bucket=bucket_name, Key=filename, Body=data)
+            print "Success: bucket '{0}' allows for uploading arbitrary files!!!".format(bucket_name.encode('utf-8'))
+            results = "http://s3.{0}.amazonaws.com/{1}/{2}\n".format(region, bucket_name, filename)
+            appendOutput(results)
+        except Exception as e:
+            print "Error: couldn't upload a {0} file to {1}. Details: {2}\n".format(filename, bucket_name, e)
+        finally:
+        	print "done"
+
+def appendOutput(results):
+    with open(OUTPUT_FILE, "a") as output:
+        output.write(results)
+
+
+def bucketWorker():
+    while True:
+        try:
+            bucket = queue.get()
+            bucketReader(bucket)
+            if WRITE_TEST_ENABLED: writeTest(bucket, WRITE_TEST_FILE)
+        except Exception as e:
+            print "Error: {0}\n".format(e)
+        queue.task_done()
+    
+
+def printHelp():
+    print('''
+--------------
+BucketScanner
+By @Rzepsky
+--------------
+======================= Notes =======================
+This tool is made for legal purpose only!!! It allows you to:
+- find collectable files for an anonymous/authenticated user in your buckets
+- verify if an anonymous/authenticated user is allowed to upload arbitrary files to your buckets
+
+
+====================== Options ======================
+-l: specify a list with bucket names to check.
+-w: specify a file to upload to a bucket.
+-r: specify a regular expression to filter the output.
+-s: look only for files bigger than 's' bytes 
+-m: look only for files smaller than 'm' bytes 
+-t: specify number of threads to use.
+-o: specify an output file.
+-h: prints a help message.
+
+====================== Example ======================
+
+$ python BucketScanner.py -l bucket_list.txt -w upload_file.txt -r '^.*\.(db|sql)' -t 50 -m 5242880 -o output.txt
+
+
+The above command will:
+- test all buckets from bucket_list.txt file
+- test if you can upload upload_file.txt to any of the bucket included in bucket_list.txt
+- provide URLs in output.txt only to files bigger than 5 MB and with .db or .sql extension
+- work on 50 threads
+''')
+
+def closingWords():
+    print "That's all folks! All collectable files can be found in {0}.".format(OUTPUT_FILE)
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("-l", dest="bucket_list", required=True, help="a list with bucket names.") 
+    parser.add_argument("-w", dest="write", type=str, required=False, default="", help="file to execute upload test.")
+    parser.add_argument("-r", dest="regex", required=False, default='', help="regular expression filter")
+    parser.add_argument("-s", dest="min", type=int, required=False, default=1, help="minimun size.")
+    parser.add_argument("-m", dest="max", type=int, required=False, default=0, help="maximum size.")
+    parser.add_argument("-t", dest="threads", type=int, required=False, default=1, help="thread count.")
+    parser.add_argument("-o", dest="output", type=str, required=False, default="output.txt", help="output file.")
+
+    if len(sys.argv) == 1:
+        printHelp()
+        sys.exit()
+    arguments = parser.parse_args()
+    if arguments.output is not "output.txt": setOutputFile(arguments.output)
+    if arguments.write: setwriteTest(arguments.write)
+    if arguments.regex: setRegex(arguments.regex)
+    if arguments.min > 1: setMinsize(arguments.min)
+    if arguments.max > 1: setMaxsize(arguments.max)
+    if not (AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY): setAnonymousMode()
+
+    arguments = parser.parse_args()
+    for i in range(0, arguments.threads):
+        t = Thread(target=bucketWorker)
+        t.daemon = True
+        t.start()    
+    
+    with open(arguments.bucket_list, 'r') as f:
+        for bucket in f:
+            queue.put(bucket.rstrip())
+        queue.join()
+
+    closingWords()
+
---
+++ README.md
@@ -0,0 +1,65 @@
+BucketScanner (by @Rzepsky)
+========================================
+
+BucketScanner is a tool used to:
+
+* find collectable files for an anonymous/authenticated user in your buckets
+* verify if an anonymous/authenticated user is allowed to upload arbitrary files to your buckets
+
+
+### Oh my gosh... another AWS bucket scanner!?
+
+Surprisingly I haven't found a one tool which has all these features:
+
+1. supports authenticated requests.
+2. checks a bucket even if you don't have 'ListBucket' permissions (I found examples when a bucket policy allows for downloading files, however in the reply to GET request to the bucket I got 403 code).
+3. the verbose mode is printed out on the terminal window while in the output file you can find URLs to only collectable files (when you work on big amounts of files it can save you a lot of time)
+4. supports test for uploading a file.
+5. supports regular expressions (to filter out only interesting files).
+6. supports minimum and maximum size filters.
+7. supports multithreading.
+
+### Usage
+
+```
+BucketScanner.py -l BUCKET_LIST [-w WRITE_TEST_FILE] [-r REGEX]  [-s MIN_SIZE] [-m MAX_SIZE] [-t THREADS] [-o OUTPUT_FILE] [-h HELP]
+
+```
+
+### Command line options
+
+* `-l <filename>` - specify a list with bucket names to check.
+* `-w <filename>` - specify a file to upload to a bucket.
+* `-r <regex expresion>` - specify a regular expression to filter the output.
+* `-s <minimum size>` - look only for files bigger than 's' bytes
+* `-m <maximum size>` - look only for files smaller than 'm' bytes 
+* `-t <threads>` - number of threads to run (default: `10`).
+* `-u <url/domain>` - full URL (including scheme), or base domain name.
+* `-v` - verbose output (show all results).
+* `-o <filename>` - specify an output file for collectable URLs.
+* `-h` - prints a help message.
+
+> Please note that to use authenticated requests you have to specify `AWS_ACCESS_KEY_ID` and `AWS_SECRET_ACCESS_KEY` variables in BucketScanner.py file.
+
+### Example
+
+```
+$ python BucketScanner.py -l bucket_list.txt -w upload_file.txt -r '^.*\.(db|sql)' -t 50 -s 5242880 -o output.txt
+```
+Using the above command, a BucketScanner will:
+
+* test all buckets from `bucket_list.txt` file
+* test if you can upload `upload_file.txt` to any of the bucket included in `bucket_list.txt`
+* provide URLs in output.txt only to files bigger than 5 MB and with `.db` or `.sql` extension
+* work on 50 threads
+
+### Pre-requisites
+To run the BucketScanner you have to install python `boto3` and `requests` libraries. You can do this by running the following command:
+
+```
+pip install -r requirements.txt
+```
+
+### License
+
+See the LICENSE file.
